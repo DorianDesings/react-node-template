2be786b4144822d1c430bf51b4d6307711cc5465
Dorian <dorianoriginaldesings@gmail.com>
2024-01-31 16:37:42 +0100

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { corsOptions } = require('./config/cors.config');
+require('dotenv').config();
 
 // Rutas
 
 // Middlewares para cliente
-// Opciones avanzadas de configuración de CORS
-const corsOptions = {
-  origin: 'http://localhost:5173', // Dominios autorizados
-  methods: '*', // Métodos permitidos
-  optionsSuccessStatus: 204
-};
 app.use(cors(corsOptions));
 app.use(express.json());
 
 // Uso de rutas
 
-app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
+const startServer = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DATABASE}`);
+    console.log('Connected to database');
+    app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
+  } catch (err) {
+    console.log('Connection error', err);
+  }
+};
+
+startServer();
