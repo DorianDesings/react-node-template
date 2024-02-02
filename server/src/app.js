bbc8ddba1924c0223c2c787c7f8466c17b145e7f
Dorian <dorianoriginaldesings@gmail.com>
2024-02-02 21:27:52 +0100

    Updated for apis

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const app = express();
+
 const { corsOptions } = require('./config/cors.config');
+const userRoutes = require('./routes/user.routes');
+const { databaseConnect } = require('./database/database');
+const authRoutes = require('./routes/auth.routes');
 require('dotenv').config();
 
 // Rutas
@@ -10,17 +14,15 @@ require('dotenv').config();
 // Middlewares para cliente
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
 // Uso de rutas
+// app.use('/api/users', userRoutes);
+// app.use('/auth', authRoutes);
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DATABASE}`);
-    console.log('Connected to database');
-    app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
-  } catch (err) {
-    console.log('Connection error', err);
-  }
+const startServer = () => {
+  databaseConnect();
+  app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
 };
 
 startServer();
